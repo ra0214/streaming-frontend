9715b4ee810e91817842f8352fc619a5fb0a1e4c
Maximiliano Diaz Clemente <233306@ids.upchiapas.edu.mx>
2026-02-26 17:49:16 -0600

    Funcionando perfecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,13 +79,7 @@ ksp {
 
 configurations.all {
     resolutionStrategy {
-        force("androidx.core:core:1.13.1")
-        force("androidx.core:core-ktx:1.13.1")
-        force("androidx.activity:activity:1.9.3")
-        force("androidx.activity:activity-compose:1.9.3")
-        force("androidx.navigation:navigation-compose:2.8.3")
-        force("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-        force("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+        force("androidx.navigation:navigation-compose:2.8.9")
     }
 }
 
